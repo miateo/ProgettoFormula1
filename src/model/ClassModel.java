aa0711b403e149ba1f1d07aad295c93a070701d8
zambonmatteo <matteo.zambon2004@gmail.com>
2022-04-07 12:54:19 +0200

    aggiunte tutte immagini piloti

--- src/model/ClassModel.java
+++ src/model/ClassModel.java
@@ -16,7 +16,7 @@ public class ClassModel {
 	private ImageIcon riciardo;
 	private ImageIcon norris;
 	private ImageIcon magnussen;
-	private ImageIcon schumuacher;
+	private ImageIcon schumacher;
 	private ImageIcon ocon;
 	private ImageIcon alonso;
 	private ImageIcon zhou;
@@ -32,10 +32,130 @@ public class ClassModel {
 	public ClassModel() {
 		try {
 			albon = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Albon.png")));
+			latifi = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Latifi.png")));
+			sainz = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Sainz.png")));
+			leclerc = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Leclerc.png")));
+			vettel = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Vettel.png")));
+			stroll = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Stroll.png")));
+			riciardo = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Riciardo.png")));
+			norris = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Norris.png")));
+			magnussen = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Magnussen.png")));
+			schumacher = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Schumacher.png")));
+			ocon = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Ocon.png")));
+			alonso = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Alonso.png")));
+			zhou = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Zhou.png")));
+			bottas = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Bottas.png")));
+			russel = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Russel.png")));
+			hamilton = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Hamilton.png")));
+			verstappen = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Verstappen.png")));
+			perez = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Perez.png")));
+			tsunoda = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Tsunoda.png")));
+			gasly = new ImageIcon(ImageIO.read(new File("assets/imgPiloti/Gasly.png")));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.err.println("img not found.");
 		}
 
 	}
+
+
+	public ImageIcon getAlbon() {
+		return albon;
+	}
+
+
+	public ImageIcon getLatifi() {
+		return latifi;
+	}
+
+
+	public ImageIcon getSainz() {
+		return sainz;
+	}
+
+
+	public ImageIcon getLeclerc() {
+		return leclerc;
+	}
+
+
+	public ImageIcon getVettel() {
+		return vettel;
+	}
+
+
+	public ImageIcon getStroll() {
+		return stroll;
+	}
+
+
+	public ImageIcon getRiciardo() {
+		return riciardo;
+	}
+
+
+	public ImageIcon getNorris() {
+		return norris;
+	}
+
+
+	public ImageIcon getMagnussen() {
+		return magnussen;
+	}
+
+
+	public ImageIcon getSchumuacher() {
+		return schumacher;
+	}
+
+
+	public ImageIcon getOcon() {
+		return ocon;
+	}
+
+
+	public ImageIcon getAlonso() {
+		return alonso;
+	}
+
+
+	public ImageIcon getZhou() {
+		return zhou;
+	}
+
+
+	public ImageIcon getBottas() {
+		return bottas;
+	}
+
+
+	public ImageIcon getRussel() {
+		return russel;
+	}
+
+
+	public ImageIcon getHamilton() {
+		return hamilton;
+	}
+
+
+	public ImageIcon getVerstappen() {
+		return verstappen;
+	}
+
+
+	public ImageIcon getPerez() {
+		return perez;
+	}
+
+
+	public ImageIcon getTsunoda() {
+		return tsunoda;
+	}
+
+
+	public ImageIcon getGasly() {
+		return gasly;
+	}
+	
 }
